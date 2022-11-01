0a8caa98c1c322661f1864d3a2247bc7a0c560b4
baileydiaz <baileydiaz10@gmail.com>
2022-11-01 13:21:33 -0600

    no changes added

---
+++ Classes.cs
@@ -0,0 +1,19 @@
+class Score{
+
+}
+
+class Player{
+
+}
+
+abstract class fallingObject{
+
+}
+
+class Rocks{
+
+}
+
+class Gems{
+    
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 ﻿using Raylib_cs;
 using System.Numerics;
 
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
