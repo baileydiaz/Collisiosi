b74569ab82991db2473d4ec57df95319dc380883
TrevorJac14 <trevorjac3@gmail.com>
2022-11-01 13:18:05 -0600

    Added movement inputs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,18 @@
 ﻿using Raylib_cs;
 using System.Numerics;
 
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+if (Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT)) {
+    PlayerRectangle.x += MovementSpeed;
+    }
+
+if (Raylib.IsKeyDown(KeyboardKey.KEY_LEFT)) {
+    PlayerRectangle.x -= MovementSpeed;
+    }
+
+if (Raylib.IsKeyDown(KeyboardKey.KEY_UP)) {
+    PlayerRectangle.y -= MovementSpeed;
+    }
+
+if (Raylib.IsKeyDown(KeyboardKey.KEY_DOWN)) {
+    PlayerRectangle.y += MovementSpeed;
+    }
