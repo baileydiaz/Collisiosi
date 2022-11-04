650cf1a7fc07b1b6e5fcbcdeaa30bd95318de538
baileydiaz <baileydiaz10@gmail.com>
2022-11-04 11:56:11 -0600

    complete almost all the classes

--- Classes.cs
+++ Classes.cs
@@ -1,19 +1,107 @@
-class Score{
+// Here we addded the proper libraries for the classes to work
 
-}
-
-class Player{
-
-}
+using Raylib_cs;
+using System.Numerics;
 
 abstract class fallingObject{
+    public Vector2 Position { get; set; } = new Vector2(0, 0);
+    public Vector2 Velocity { get; set; } = new Vector2(0, 0);
 
+    virtual public void Draw() {
+        // Base game objects do not have anything to draw
+    }
+
+    public void Move() {
+        Vector2 NewPosition = this.Position;
+        NewPosition.X += Velocity.X;
+        NewPosition.Y += Velocity.Y;
+        Position = NewPosition;
 }
 
-class Rocks{
 
+
+public class ColoredObject: fallingObject {
+    public Color Color { get; set; }
+
+    public ColoredObject(Color color) {
+        Color = color;
+    }
 }
 
-class Gems{
+public class Gems: ColoredObject {
+    public int Size { get; set; }
+
+    public Gems(Color color, int size): base(color) {
+        Size = size;
+    }
+
+public class Rocks: ColoredObject {
+    public int Size { get; set; }
+
+    public Rocks(Color color, int size): base(color) {
+        Size = size;
+    }
+
+    override public void Draw() {
+        Raylib.DrawRectangle((int)Position.X, (int)Position.Y, Size, Size, Color);
+    }
+}
+    // set the shape to rectangle 
     
+}
+
+
+    override public void Draw() {
+        Raylib.DrawRectangle((int)Position.X, (int)Position.Y, Size, Size, Color);
+    }
+}
+
+
+public class Player{
+    // creates a circle for the "player"
+    int ScreenHeight = 480;
+    int ScreenWidth = 800;
+    int RectangleSize = 50;
+    int MovementSpeed = 5;
+    public Player(){
+        
+        PlayerRectangle = new Rectangle(ScreenWidth - (RectangleSize * 2), ScreenHeight - (RectangleSize * 2), RectangleSize, RectangleSize);
+    }
+    Rectangle PlayerRectangle;
+
+    public void inputs(){
+        if (Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT)) {
+            PlayerRectangle.x += MovementSpeed;
+            }
+
+        if (Raylib.IsKeyDown(KeyboardKey.KEY_LEFT)) {
+            PlayerRectangle.x -= MovementSpeed;
+            }}
+    // set a start a postion
+
+
+    // use player movement code
+}
+    
+class Score{
+    // set score attribute to 0 and use a constructor to do so
+    int score = 0;
+    
+    public Score(){
+    }
+
+    // methods to add or subtract from score
+    public void AddtoScore(int score){
+        score +=1;
+    }
+
+    // it is a method that will subtract 1 from the score if that happens
+    public void SubtractfromScore(int score){
+    score += 1;
+    }
+
+    // get the score from the object and display it wit4h the raylib method
+    public void displayScore(){
+        Console.Write("Score:${score}");
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,18 +1,44 @@
 ﻿using Raylib_cs;
 using System.Numerics;
 
-if (Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT)) {
-    PlayerRectangle.x += MovementSpeed;
-    }
+    static class Program{
+        public static void Main()
+        {
 
-if (Raylib.IsKeyDown(KeyboardKey.KEY_LEFT)) {
-    PlayerRectangle.x -= MovementSpeed;
-    }
+            var ScreenHeight = 480;
+            var ScreenWidth = 800;
+            var RectangleSize = 50;
 
-if (Raylib.IsKeyDown(KeyboardKey.KEY_UP)) {
-    PlayerRectangle.y -= MovementSpeed;
-    }
+            var PlayerRectangle = new Rectangle(ScreenWidth - (RectangleSize * 2), ScreenHeight - (RectangleSize * 2), RectangleSize, RectangleSize);
+            var TargetRectangle = new Rectangle(100, 100, RectangleSize, RectangleSize);
+            var MovementSpeed = 4;
 
-if (Raylib.IsKeyDown(KeyboardKey.KEY_DOWN)) {
-    PlayerRectangle.y += MovementSpeed;
+            Raylib.InitWindow(ScreenWidth, ScreenHeight, "Ball");
+            Raylib.SetTargetFPS(60);
+
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.BLACK);
+
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT)) {
+                    PlayerRectangle.x += MovementSpeed;
+                }
+
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_LEFT)) {
+                    PlayerRectangle.x -= MovementSpeed;
+                }
+
+                Raylib.DrawRectangleRec(TargetRectangle, Color.BLUE);
+                Raylib.DrawRectangleRec(PlayerRectangle, Color.RED);
+
+                if (Raylib.CheckCollisionRecs(PlayerRectangle, TargetRectangle)) {
+                    Raylib.DrawText("You did it!!!!", 12, 34, 20, Color.BLACK);
+                }
+
+                Raylib.EndDrawing();
+            }
+
+            Raylib.CloseWindow();
+        }
     }
